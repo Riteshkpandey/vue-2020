d92ca015a41e492f699a826c51a4f87160d84dc3
riteshk615 <riteshk615@gmail.com>
2020-12-07 22:33:06 +0530

    first commit

---
+++ src/components/login.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <input type="text" v-model="loginform.username" name="user" placeholder="Enter user id"/>
+        <br>
+        <input type="password" v-model="loginform.password"  name="password" placeholder="enter password"/>
+        <br>
+        <button v-on:click="logindata">login</button>
+    </div>
+</template>
+<script>
+export default {
+    name:"login",
+    methods:{
+        logindata(){
+            console.log(this.loginform.username,this.loginform.password)
+
+        }
+        
+        },
+    data(){
+        return {loginform:{
+            username:null,
+            password:null,
+        }}
+    }
+}
+</script>
